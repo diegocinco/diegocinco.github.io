c11ef1ad57292a9d3d74e5911e3e893b8e728910
diegocinco <diegocinco5@gmail.com>
2023-03-03 15:02:31 -0300

    initial commit

---
+++ scripts.js
@@ -0,0 +1,26 @@
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activar scrollspy en el elemento nav principal
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            offset: 74,
+        });
+    };
+
+    // Contraer la barra de navegación cuando el toggler está visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
